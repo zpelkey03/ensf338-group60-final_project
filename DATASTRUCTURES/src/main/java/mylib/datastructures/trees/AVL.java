76cd61b03a99e4c1b330b0f8681c4ad9873d9930
d-anez3 <david.anez@ucalgary.ca>
2023-04-07 21:43:05 -0600

    idk if it works but AVL is 'done'

--- DATASTRUCTURES/src/main/java/mylib/datastructures/trees/AVL.java
+++ DATASTRUCTURES/src/main/java/mylib/datastructures/trees/AVL.java
@@ -16,9 +16,9 @@ public class AVL extends BST{
 
     public AVL(TNode obj) {
         super(obj);
-        // If the TNode obj has children, the constructor needs to create a balanced tree from passed tree by one 
-        // of the two following options: iteratively inserting nodes from the original tree and balancing the 
-        // new created AVL tree or implementing a full tree balancing algorithm (Bonus)
+        if (obj.getLeft() != null || obj.getRight() != null) {
+            balance(root);
+        }
     }
 
     public TNode getRoot() {
@@ -27,31 +27,74 @@ public class AVL extends BST{
 
     public void setRoot(TNode root) {
         this.root = root;
-        // the setter function must check if the node has children. If children are found it must do the same
-        // as the overload constructor.
+        if (root.getLeft() != null || root.getRight() != null) {
+            balance(root);
+        }
     }
 
 
     public void insert(TNode node){
-        // inserts the node passed as argument into the tree
-        // Must maintain the tree balance. It can call the super.insert (insert function from BST), but will need to also balance the tree after
+        super.insert(node);
+        balance(root);
     }
 
     public void insert(int val) {
-        // creates a new node with data val to be inserted into the tree
-        // Must maintain the tree balance. It can call the super.insert (insert function from BST), but will need to also balance the tree after
+        super.insert(val);
+        balance(root);
     }
 
     public void delete(int val) {
-        TNode search = search(val);
-        if (search != null) {
-            // delete the node
-            // balance the tree
-        } else {
+        TNode node = search(val);
+        if (node == null) {
             System.out.println("Value not found in tree");
+            return;
         }
-        // finds the node with val as data and deletes it, if not found prints a statement that the value is not in the tree
+        root = deleteNode(root, val);
+        balance(root);
     }
+    
+    private TNode deleteNode(TNode node, int val) {
+        if (node == null) {
+            return node;
+        }
+        if (val < node.getData()) {
+            node.setLeft(deleteNode(node.getLeft(), val));
+        } else if (val > node.getData()) {
+            node.setRight(deleteNode(node.getRight(), val));
+        } else {
+            if (node.getLeft() == null || node.getRight() == null) {
+                TNode temp;
+                if (node.getLeft() == null) {
+                    temp = node.getRight();
+                } else {
+                    temp = node.getLeft();
+                }
+                if (temp == null) {
+                    temp = node;
+                    node = null;
+                } else {
+                    node = temp;
+                }
+            } else {
+                TNode temp = minimumValueNode(node.getRight());
+                node.setData(temp.getData());
+                node.setRight(deleteNode(node.getRight(), temp.getData()));
+            }
+        }
+        if (node == null) {
+            return node;
+        }
+        updateHeight(node);
+        return balance(node);
+    }
+    
+    private TNode minimumValueNode(TNode node) {
+        TNode current = node;
+        while (current.getLeft() != null) {
+            current = current.getLeft();
+        }
+        return current;
+    }    
 
     public TNode search(int val) {
         return super.search(val);
@@ -65,9 +108,61 @@ public class AVL extends BST{
         super.printBF();
     }
 
-    private void balance() {
+    private void updateHeight(TNode node) {
+        int leftNodeBalance = node.getLeft().getBalance();
+        int rightNodeBalance = node.getRight().getBalance();
+        node.setBalance(Math.max(leftNodeBalance, rightNodeBalance) + 1);
+    }
+
+    private int balanceFactor(TNode node) {
+        if (node == null) {
+            return -1;
+        }
+        return node.getLeft().getBalance() - node.getRight().getBalance();
+    }
+
+    private TNode rotateLeft(TNode node) {
+        TNode leftNode = node.getLeft();
+        node.setLeft(leftNode.getRight());
+        leftNode.setRight(node);
+        updateHeight(node);
+        updateHeight(leftNode);
+        return leftNode;
 
     }
 
-    // Hint: implement the balance function as a helper function (private function) and call it in the insert (and Delete if you attempt the bonus)
+    private TNode rotateRight(TNode node) {
+        TNode rightNode = node.getRight();
+        node.setRight(rightNode.getLeft());
+        rightNode.setLeft(node);
+        updateHeight(node);
+        updateHeight(rightNode);
+        return rightNode;
+    }
+
+    private TNode balance(TNode node) {
+        int balanceFactor = balanceFactor(node);
+
+        if (balanceFactor < -1) {
+            if (balanceFactor(node.getLeft()) <= 0) {
+                node = rotateRight(node);
+            }
+            else {
+                node.setLeft(rotateLeft(node.getLeft()));
+                node = rotateRight(node);
+            }
+        }
+
+        if (balanceFactor > 1) {
+            if (balanceFactor(node.getRight()) >= 0) {
+                node = rotateLeft(node);
+            }
+            else {
+                node.setRight(rotateRight(node.getRight()));
+                node = rotateLeft(node);
+            }
+        }
+
+        return node;
+    }
 }
